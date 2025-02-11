79c8d66e701fa508eb5bad51c8852ab30a75d85b
domicel <domicita098@gmail.com>
2025-02-10 20:13:02 -0400

    primer conmit

---
+++ android/app/src/main/kotlin/com/example/impresion_zpl/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.impresion_zpl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+//import 'package:esc_pos_utils/esc_pos_utils.dart';
+//import 'package:esc_pos_printer/esc_pos_printer.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ImpresionArchivosZpl(),
+    );
+  }
+}
+
+class ImpresionArchivosZpl extends StatefulWidget {
+
+  @override
+  _ImpresionArchivosZplState createState() => _ImpresionArchivosZplState();
+}
+class _ImpresionArchivosZplState extends State<ImpresionArchivosZpl> {
+  String filePath= '/storage/emulated/0/Android/data/com.example.impresion_zpl/files/downloads/Etiqueta_producto.txt';
+  BluetoothDevice? zebraPrinter;
+  BluetoothCharacteristic? writeCharacteristic;
+  // String zplCode = '''
+  // ^XA
+  // ^FO50,60^A0,40^FDWorld's Best Griddle^FS
+  // ^FO60,120^BY3^BCN,60,,,,A^FD1234ABC^FS
+  // ^FO25,25^GB380,200,2^FS
+  // ^XZ
+  // ''';
+
+  void imprimirArchivo() async {
+    //  Directory? directorioBase = await getDownloadsDirectory();
+    //  directorioBase ??= await getApplicationDocumentsDirectory();
+    //  String directoryPath = p.join(directorioBase.path,'factura_zebra_1.pdf',);
+     File file = File(filePath);
+      if (file.existsSync()) { //verificar existencia de archivo
+        buscarImpresora();
+      } else {
+        _showDialog("Archivo No Encontrado", "No se encontró el archivo $filePath");
+      }
+  }
+  void buscarImpresora() async {
+    var status = await Permission.location.request();
+    if (status.isGranted){
+      debugPrint("-----iniciando-escaneo---------");
+      FlutterBluePlus.startScan(timeout: Duration(seconds: 50));
+      FlutterBluePlus.scanResults.listen((List<ScanResult> results) {
+        // Imprimir todos los resultados de escaneo para depuración
+        debugPrint("Resultados del escaneo: ${results.map((r) => r.device.platformName).toList()}");
+        for (ScanResult r in results) {
+          if (r.device.platformName.contains("ZQ-ELSA")){
+            zebraPrinter = r.device;
+            FlutterBluePlus.stopScan();
+            break;
+          }
+        }
+      });
+      if (zebraPrinter!=null){
+        await zebraPrinter!.connect();
+        _showDialog("Conectado", "Conectado a la impresora ${zebraPrinter!.platformName}");
+        enviarAImpresora();
+        //await zebraPrinter!.disconnect();
+      }else{
+         _showDialog("No se encontraron impresoras", "No se encontraron impresoras Zebra.");
+      }
+    } else {
+    _showDialog("Permiso Denegado", "Se necesitan permisos de ubicación para escanear dispositivos Bluetooth.");
+    }
+  }
+   void enviarAImpresora() async {
+    try{
+      List<BluetoothService> services = await zebraPrinter!.discoverServices();
+      for (BluetoothService service in services) {
+        for (BluetoothCharacteristic characteristic in service.characteristics) {
+          if (characteristic.properties.write) {
+            debugPrint("Característica de escritura encontrada: ${characteristic.uuid}");
+            debugPrint("UUID: ${characteristic.uuid} - Propiedades: ${characteristic.properties}");
+            writeCharacteristic = characteristic;
+            await enviarArchivo(characteristic);
+            break;
+          }
+        }
+      }
+    }catch (e) {
+      _showDialog("ERROR","Error al enviar el archivo: $e");
+    }
+    
+  }
+  Future<void> enviarArchivo(BluetoothCharacteristic characteristic) async {
+  try {
+    File file = File(filePath);
+    String zplCode = await file.readAsString();
+    List<int> zplBytes = utf8.encode(zplCode);
+    await writeCharacteristic!.write(zplBytes, withoutResponse: false);
+  } catch (e) {
+     debugPrint("eerrror: $e");
+    _showDialog("ERROR","❌ Error al enviar el archivo: $e");
+  }
+}
+  void _showDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("imprimir de archivos")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: imprimirArchivo,//findFile,
+          child: Text("Imprimir en Zebra"),
+        ),
+      ),
+    );
+  }
+}
